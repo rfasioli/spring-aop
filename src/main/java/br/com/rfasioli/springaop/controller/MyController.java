091dbedd39ee4a0cb3959e80ce35058bd47bc375
rfasioli <rfasioli@uolinc.com>
2021-05-13 19:20:35 -0300

    Ajuste na controller para retornar o response entity

--- src/main/java/br/com/rfasioli/springaop/controller/MyController.java
+++ src/main/java/br/com/rfasioli/springaop/controller/MyController.java
@@ -1,11 +1,14 @@
 package br.com.rfasioli.springaop.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 import br.com.rfasioli.springaop.aspect.IOActions;
 import br.com.rfasioli.springaop.service.MyService;
@@ -29,8 +32,8 @@ public class MyController {
 
     @IOActions(args = {"0"})
     @PostMapping("")
-    public String save(@RequestBody final String payload) {
-        return "Hello";
+    public ResponseEntity<String> save(@RequestBody final String payload) {
+        return ResponseEntity.of(Optional.of("Hello"));
     }
 
 }
